aef0d08a9b795b078ebcaed6f020a795d409d5b8
scynthi <85178488+scynthi@users.noreply.github.com>
2025-11-07 13:13:40 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,8 +9,20 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            if (args[0] == "--ui")
+            {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args); 
+            }   
+        }
+        else
+        {
+            Console.WriteLine("Running in console mode");
+        }
+    } 
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
