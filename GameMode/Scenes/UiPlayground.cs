db1e7f24c8f12afef3d35243668cac9dde4ace5c
scynthi <85178488+scynthi@users.noreply.github.com>
2025-11-19 21:16:41 +0100

    ui updates

---
+++ GameMode/Scenes/UiPlayground.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class UiPlayground : Control
+{
+    [Export] PackedScene cardUIScene;
+
+    public override void _Ready()
+    {
+        UICard uiCard = (UICard)cardUIScene.Instantiate();
+        AddChild(uiCard);
+        uiCard.EditAllCardInformation("Albert", 10, 3, true, true);
+
+    }
+}
