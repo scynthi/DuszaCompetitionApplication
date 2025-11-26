c28bccc300917b9dd0b063f25a72e929d5b61d97
PITRv1 <petersandor.toth@gmail.com>
2025-11-26 16:22:08 +0100

    Added better shop

--- GameMode/Scenes/ShopScene/ShomMenu.cs
+++ GameMode/Scenes/ShopScene/ShomMenu.cs
@@ -3,16 +3,26 @@ using System;
 
 public partial class ShomMenu : Control
 {
+    [Export] Label moneyLabel;
+
+    public override void _Ready()
+    {
+        UpdateMoneyText();
+    }
+
     public void GiveItemToPlayer(ItemType type)
     {
-
         IItem item = Items.CreateItemFromType(type);
         if (Global.gameManager.saverLoader.currSaveFile.player.Money < item.Price) return;
 
         Global.gameManager.saverLoader.currSaveFile.player.Money -= item.Price;
         Global.gameManager.saverLoader.currSaveFile.player.AddToItemList(item);
     
-        GD.Print(Global.gameManager.saverLoader.currSaveFile.player.Money);
+        UpdateMoneyText();
+    }
 
+    public void UpdateMoneyText()
+    {
+        moneyLabel.Text = Convert.ToString(Global.gameManager.saverLoader.currSaveFile.player.Money);
     }
 }
