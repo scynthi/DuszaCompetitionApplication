d784b78f0788eb49992cdb13c264199305bb138f
borokmate <>
2025-11-30 12:48:11 +0100

    good

--- GameMode/Scripts/WorldObject/Card.cs
+++ GameMode/Scripts/WorldObject/Card.cs
@@ -3,26 +3,26 @@ using System;
 
 public partial class Card : IWorldObject
 {
-	public string Name { set; get; }
+    public string Name { set; get; }
     public int Health { set; get; }
     public int BaseDamage { set; get; }
     public int Damage { set; get; }
-    public bool DamageChanged {set; get;}
-    public bool HealthChanged {set; get;}
+    public bool DamageChanged { set; get; }
+    public bool HealthChanged { set; get; }
     public CardElements CardElement { set; get; }
     public string Icon { set; get; }
     public BuffHandler buffHandler { set; get; }
 
-    public Card(){}
+    public Card() { }
 
     public Card(string name, int baseDamage, int health, CardElements cardElements, string cardIcon)
     {
-        
-        Name        = name;
-        Health      = health;
-        Icon        = cardIcon;
-        BaseDamage  = baseDamage;
-        Damage      = baseDamage;
+
+        Name = name;
+        Health = health;
+        Icon = cardIcon;
+        BaseDamage = baseDamage;
+        Damage = baseDamage;
         CardElement = cardElements;
         buffHandler = new BuffHandler();
         buffHandler.BindCard(this);
@@ -30,27 +30,27 @@ public partial class Card : IWorldObject
 
     public Card(string name, int baseDamage, int health, CardElements cardElements)
     {
-        Name        = name;
-        Health      = health;
-        BaseDamage  = baseDamage;
-        Damage      = baseDamage;
+        Name = name;
+        Health = health;
+        BaseDamage = baseDamage;
+        Damage = baseDamage;
         CardElement = cardElements;
         buffHandler = new BuffHandler();
         buffHandler.BindCard(this);
     }
-	
-	public Card(Card other)
+
+    public Card(Card other)
     {
-        Name          = other.Name;
-        Health        = other.Health;
-        Icon          = other.Icon;
-        BaseDamage    = other.BaseDamage;
-        Damage        = other.BaseDamage;
-        CardElement   = other.CardElement;
+        Name = other.Name;
+        Health = other.Health;
+        Icon = other.Icon;
+        BaseDamage = other.BaseDamage;
+        Damage = other.BaseDamage;
+        CardElement = other.CardElement;
         DamageChanged = other.DamageChanged;
         HealthChanged = other.HealthChanged;
-        Icon          = other.Icon;
-        buffHandler   = new BuffHandler();
+        Icon = other.Icon;
+        buffHandler = new BuffHandler();
         buffHandler.BindCard(this);
     }
 
@@ -62,19 +62,19 @@ public partial class Card : IWorldObject
     public void ModifyHealth(int value)
     {
         Health = Math.Clamp(Health + value, 0, 100);
-        HealthChanged = true; 
+        HealthChanged = true;
     }
     public void SetBaseDamage(int value)
     {
-        BaseDamage = Math.Clamp(value, 0, 100); 
+        BaseDamage = Math.Clamp(value, 0, 100);
     }
     public void SetDamage(int value)
     {
-        Damage = Math.Clamp(value, 0, 100); 
+        Damage = Math.Clamp(value, 0, 100);
     }
     public void SetHealth(int value)
     {
-        Health = Math.Clamp(value, 0, 100); 
+        Health = Math.Clamp(value, 0, 100);
     }
     public int Attack(Card card, bool PlayerAttacked)
     {
@@ -87,10 +87,19 @@ public partial class Card : IWorldObject
         Random rnd = new Random();
 
         int damage = Convert.ToInt32(Math.Round(value * buffHandler.CalculateDamageTakenMultiplier()));
-        double difficultyModifier = PlayerAttacked ? 1 + rnd.NextDouble() * ((double)Global.gameManager.saverLoader.currSaveFile.gameDifficulty / 10) : 1 - rnd.NextDouble() * ((double)Global.gameManager.saverLoader.currSaveFile.gameDifficulty / 20);
-
+        double difficultyModifier;
+        if (PlayerAttacked)
+        {
+            difficultyModifier = 1 + rnd.NextDouble() * ((double)Global.gameManager.saverLoader.currSaveFile.gameDifficulty / 10);
+        }
+        else
+        {
+            difficultyModifier = 1 - rnd.NextDouble() * ((double)Global.gameManager.saverLoader.currSaveFile.gameDifficulty / 20);
+        }
+        GD.Print("Damage: " + damage);
+        GD.Print("modifier: " + difficultyModifier);
         damage = Convert.ToInt32(Math.Round(damage * difficultyModifier));
-
+        GD.Print("Damage2: " + damage);
 
         Health -= damage;
         Health = Math.Clamp(Health, 0, 100);
