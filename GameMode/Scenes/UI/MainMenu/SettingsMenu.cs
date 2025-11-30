318923f11fd1053464d6bd137183cd4d2a5c2292
PITRv1 <petersandor.toth@gmail.com>
2025-11-30 01:25:22 +0100

    made basic settings screen

---
+++ GameMode/Scenes/UI/MainMenu/SettingsMenu.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class SettingsMenu : Control
+{
+    [Export] Slider sfxSlider;
+    [Export] Slider musicSlider;
+
+
+    public void ReloadSliders()
+    {
+        sfxSlider.Value = Global.gameManager.audioController.sfxAudioPlayer.VolumeDb;
+        musicSlider.Value = Global.gameManager.audioController.musicAudioPlayer.VolumeDb;
+    }
+
+
+    public void _SFXVolumeChanged(float value)
+    {
+        Global.gameManager.audioController.sfxAudioPlayer.VolumeDb = value;
+        Global.gameManager.audioController.envAudioPlayer.VolumeDb = value;
+    }
+
+    public void _MusicVolumeChanged(float value)
+    {
+        Global.gameManager.audioController.musicAudioPlayer.VolumeDb = value;
+    }
+
+    public void _CrtToggeled(bool status)
+    {
+        Global.gameManager.ctrShaderRect.Visible = status;
+    }
+}
